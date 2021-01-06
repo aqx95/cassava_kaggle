48f32cec8675b31b304a5001f4b281add926051f
aqx95 <angqx95@gmail.com>
2021-01-06 23:07:51 +0800

    loss function

--- loss.py
+++ loss.py
@@ -1,5 +1,5 @@
 import torch.nn as nn
 
 def loss_fn(loss_name, config):
-    if loss_name == 'crossentropy':
+    if loss_name == "crossentropy":
         return nn.CrossEntropyLoss(**config.criterion_params[config.criterion])
