22c74f0c6fdb368eed2c0e767dd697b6aaca1eb2
aqx95 <angqx95@gmail.com>
2021-02-14 19:06:28 +0800

    Add Resnet base model

--- model.py
+++ model.py
@@ -5,9 +5,9 @@ class Effnet(nn.Module):
     """
     EfficientNet model by https://arxiv.org/pdf/1905.11946.pdf
     """
-    def __init__(self, model_name, n_class, pretrained=False):
+    def __init__(self, model_name, n_class, drop_rate=0.0, pretrained=False):
         super().__init__()
-        self.model = timm.create_model(model_name, pretrained=pretrained)
+        self.model = timm.create_model(model_name, drop_rate=drop_rate, pretrained=pretrained)
         n_features = self.model.classifier.in_features
         self.model.classifier = nn.Linear(n_features, n_class)
 
@@ -20,9 +20,9 @@ class ViT(nn.Module):
     """
     VisionTransformer model by https://arxiv.org/pdf/2010.11929.pdf
     """
-    def __init__(self, model_name, n_class, pretrained=False):
+    def __init__(self, model_name, n_class, drop_rate=0.0, pretrained=False):
         super().__init__()
-        self.model = timm.create_model(model_name, pretrained=pretrained)
+        self.model = timm.create_model(model_name, drop_rate=drop_rate, pretrained=pretrained)
         self.model.head = nn.Linear(self.model.head.in_features, n_class)
 
     def forward(self, x):
@@ -31,9 +31,21 @@ class ViT(nn.Module):
 
 
 class Resnext(nn.Module):
-    def __init__(self, model_name, n_class, pretrained=False):
+    def __init__(self, model_name, n_class, drop_rate=0.0, pretrained=False):
           super().__init__()
-          self.model = timm.create_model(model_name, pretrained=pretrained)
+          self.model = timm.create_model(model_name, drop_rate=drop_rate, pretrained=pretrained)
+          n_features = self.model.fc.in_features
+          self.model.fc = nn.Linear(n_features, n_class)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+
+class Resnet(nn.Module):
+    def __init__(self, model_name, n_class, drop_rate=0.0, pretrained=False):
+          super().__init__()
+          self.model = timm.create_model(model_name, drop_rate=drop_rate, pretrained=pretrained)
           n_features = self.model.fc.in_features
           self.model.fc = nn.Linear(n_features, n_class)
 
@@ -46,15 +58,20 @@ class Resnext(nn.Module):
 def create_model(config):
     if config.model == "effnet":
         model_obj = Effnet
-        model = Effnet(config.model_name, config.num_classes, config.pretrained)
+        model = Effnet(config.model_name, config.num_classes, config.drop_rate, config.pretrained)
         return model
 
     if config.model == 'vit':
         model_obj = ViT
-        model = ViT(config.model_name, config.num_classes, config.pretrained)
+        model = ViT(config.model_name, config.num_classes, config.drop_rate, config.pretrained)
         return model
 
     if config.model == 'resnext':
         model_obj = Resnext
-        model = Resnext(config.model_name, config.num_classes, config.pretrained)
+        model = Resnext(config.model_name, config.num_classes, config.drop_rate, config.pretrained)
+        return model
+
+    if config.model == 'resnet':
+        model_obj = Resnet
+        model = Resnet(config.model_name, config.num_classes, config.drop_rate, config.pretrained)
         return model
