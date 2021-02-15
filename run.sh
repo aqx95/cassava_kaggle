2f7912c3932e0f6f4630cfc973a201a617cb1335
aqx95 <angqx95@gmail.com>
2021-02-15 21:33:53 +0800

    Run training script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+#Resnext_50_32x4d
+python main.py --epochs 15 --criterion symmetriccrossentropy  --optimizer Adam --image-size 512 --model-type resnext --model-name resnext50_32x4d
+
+#Inception_resnet_v2
+python main.py --epochs 15 --criterion labelsmoothloss --image-size 512 --model-type resnet --model-name inception_resnet_v2
+
+#EfficientNet B4(600x600)
+python main.py --epochs 15 --criterion bitemperedloss --image-size 600 --model-type effnet --model-name tf_efficientnet_b4_ns
+
+#Vit_base16_384
+python main.py --epochs 15 --criterion labelsmoothloss --image-size 384 --model-type vit --model-name vit_base_patch16_384
