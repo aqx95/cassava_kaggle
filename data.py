f7233379d8cb3edd53f7658bf62009c613b8057a
aqx95 <angqx95@gmail.com>
2021-02-15 12:02:49 +0800

    main training

--- data.py
+++ data.py
@@ -59,7 +59,6 @@ def get_train_transforms(config):
             ShiftScaleRotate(p=0.5),
             Rotate(p=0.5),
             CenterCrop(config.image_size, config.image_size, p=0.5),
-            CLAHE(p=0.4),
             HueSaturationValue(hue_shift_limit=0.2, sat_shift_limit=0.2, val_shift_limit=0.2, p=0.4),
             RandomBrightnessContrast(brightness_limit=(-0.1,0.1), contrast_limit=(-0.1, 0.1), p=0.4),
             Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], max_pixel_value=255.0, p=1.0),
